6b76a57f76cd6afa66bd3e91853ed2bf9060f731
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-03 22:04:18 +0600

    Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,20 @@ import MoviePage from './pages/MoviePage';
 import Netflix from './pages/Netflix';
 import Player from './pages/Player';
 import TvShow from './pages/TvShow';
+import Header from './components/Header';
 function App() {
 
   return (
     <BrowserRouter>
      <Routes>
-        
         <Route exact path='/' element={<Netflix/>}></Route>
         <Route exact path='/login' element={<LoginPage/>}></Route>
         <Route exact path='/signup' element={<SignUpPage/>}></Route>
         <Route exact path='/player' element={<Player/>}></Route>
         <Route exact path='/tv' element={<TvShow/>}></Route>
         <Route exact path='/movie' element={<MoviePage/>}></Route>
-        
       </Routes>
+      <Header/>
     </BrowserRouter>
   )
 }
