13601b53f47147f34cc921b9596b7ee9a44a1959
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-03 18:23:54 +0600

    App initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <main>
+      <div>
+        <h3>Hello handsome</h3>
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default App
