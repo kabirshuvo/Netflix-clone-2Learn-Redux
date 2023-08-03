7ba76cce58720ea0d9ee2a4da5e81ca1c2a25f85
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-03 21:54:26 +0600

    Router setup and backGroundImage component tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import './App.css'
-
+import {
+  BrowserRouter,
+  Routes, Route,
+ 
+} from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import MoviePage from './pages/MoviePage';
+import Netflix from './pages/Netflix';
+import Player from './pages/Player';
+import TvShow from './pages/TvShow';
 function App() {
 
   return (
-    <>
-    <main>
-      <div>
-        <h3>Hello handsome</h3>
-      </div>
-    </main>
-    </>
+    <BrowserRouter>
+     <Routes>
+        
+        <Route exact path='/' element={<Netflix/>}></Route>
+        <Route exact path='/login' element={<LoginPage/>}></Route>
+        <Route exact path='/signup' element={<SignUpPage/>}></Route>
+        <Route exact path='/player' element={<Player/>}></Route>
+        <Route exact path='/tv' element={<TvShow/>}></Route>
+        <Route exact path='/movie' element={<MoviePage/>}></Route>
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
