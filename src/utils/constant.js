6a61e2a020cf1577b2edd58c6197effac23f300d
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-04 17:46:10 +0600

    TMDB API key activated

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
-export const MY_API_KEY = "76d317c5cfdb5696af549bc100353756";
+export const MY_API_KEY = "b5665c387d9300bdf2b69dd3b2950c29";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
